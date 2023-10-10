5ab2b5b6369a569b41313eacca306061205e7715
马红军 <144530306+hongjun-ma@users.noreply.github.com>
2023-10-10 22:36:45 +0800

    86. 分隔链表

---
+++ 链表/86. 分隔链表.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(-1);
+        ListNode dummy2 = new ListNode(-1);
+        ListNode p1 = dummy1, p2 = dummy2;
+        ListNode p = head;
+
+        while(p!=null){
+            if(p.val < x){
+                p1.next = p;
+                p1 = p1.next;
+            }else{
+                p2.next = p;
+                p2 = p2.next;
+            }
+            ListNode temp = p.next;
+            p.next = null;
+            p = temp;
+        }
+
+        p1.next = dummy2.next;
+
+        return dummy1.next;
+
+    }
+}
