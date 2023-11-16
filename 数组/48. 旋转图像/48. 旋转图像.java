e0322c1a330583203d81108cf335ad1fcb7b0593
马红军 <144530306+hongjun-ma@users.noreply.github.com>
2023-11-16 10:45:15 +0800

    48

---
+++ 数组/48. 旋转图像/48. 旋转图像.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+
+            }
+        }
+        for(int[] row : matrix){
+            reverse(row);
+        }
+    }
+    int[] reverse(int[] arr){
+        int i = 0, j = arr.length - 1;
+        while(i <= j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        return arr;
+    }
+}
