9fab43bc5cc29718f13c5c3e9d90dd739d49186c
马红军 <144530306+hongjun-ma@users.noreply.github.com>
2024-01-16 23:03:34 +0800

    Add files via upload

---
+++ 104. 二叉树的最大深度/104. 二叉树的最大深度.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int ld = maxDepth(root.left);
+        int rd = maxDepth(root.right);
+
+        return ld>rd?ld +1:rd+1;
+        //return 1 + Math.max(ld, rd);
+
+    }
+}
